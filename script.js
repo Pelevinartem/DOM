f86ad2c0e967d6419a5c8a28ee393124d983f738
Pelevinartem <pelevin23@mail.ru>
2023-02-02 20:46:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let push = document.querySelector('.button');
+
+let i = 0;
+
+
+push.onclick = function () {
+
+    document.querySelector('.counter').innerHTML = i++;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(count);
+console.log(push);
