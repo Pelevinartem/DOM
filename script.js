5a795a08034fa712f1acc7bea4b7ec3d6482fa17
Pelevinartem <pelevin23@mail.ru>
2023-02-05 17:18:43 +0300

    add Timer

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-let push = document.querySelector('.button');
+const push = document.querySelector('.button');
+const timerId = document.querySelector('.timer');
+const counter = document.querySelector('.counter');
 
+let check = true;
 let i = 0;
 
+const TIMEOUT = 5000;
 
-push.onclick = function () {
+push.onclick = start;
 
-    document.querySelector('.counter').innerHTML = i++;
+function start() {
+
+    counter.innerHTML = i++;
+
+    if (check == true) {
+        timer();
+        check = false;
+    }
 
 };
 
+function timer() {
+    let timeNow = Date.now();
+
+    timerId.innerHTML = formatTime(TIMEOUT);
+
+    let interval = setInterval(() => {
+        let delta = Date.now() - timeNow;
+        timerId.innerHTML = formatTime(TIMEOUT - delta);
+    }, 100);
+
+    let timeout = setTimeout(() => {
+        push.onclick = null;
+        timerId.innerHTML = 'Game Over';
 
 
+        clearInterval(interval);
+        clearTimeout(timeout);
+    }, TIMEOUT);
+}
 
+function formatTime(ms) {
+    return Number.parseFloat(ms / 1000).toFixed(2);
 
-
-
-
-
-
-
-
-
-console.log(count);
-console.log(push);
+};
